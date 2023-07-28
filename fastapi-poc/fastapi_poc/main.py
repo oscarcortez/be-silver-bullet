12a23bf4e5c1410d1ecc539dbd448e4dee3694a5
oscar-cortez-home <oscarkortez@gmail.com>
2023-07-28 16:43:48 -0400

    test paths issue

--- fastapi-poc/fastapi_poc/main.py
+++ fastapi-poc/fastapi_poc/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
-from models.movie import Movie
-from config.database import Session, engine, Base
+#from models.movie import Movie
+#from config.database import Session, engine, Base
+from newdirectory.archivo import algo
 
 app = FastAPI(
     title="aplicacion con fastapi",
     version="0.0.1"
 )
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
+
+xx = algo()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": f'Hello World{xx.printer()}'}
 
---
+++ fastapi-poc/fastapi_poc/newdirectory/archivo.py
@@ -0,0 +1,3 @@
+class algo:
+    def printer():
+        return 'algo'
