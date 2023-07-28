1c8a0eb807343b32bc16f79932b0853a087708ab
oscar-cortez-home <oscarkortez@gmail.com>
2023-07-28 16:26:03 -0400

    sqlalchemy with postgres added - not work

---
+++ fastapi-poc/fastapi_poc/config/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import URL
+
+url = URL.create(
+    drivername="postgresql",
+    username='postgres',
+    host='localhost',
+    port=5437,
+    database='dvdrental',
+    password='postgres'
+)
+
+engine = create_engine(url)
+
+Session = sessionmaker(bind = engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
--- fastapi-poc/fastapi_poc/main.py
+++ fastapi-poc/fastapi_poc/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from models.movie import Movie
+from config.database import Session, engine, Base
 
-app = FastAPI()
+app = FastAPI(
+    title="aplicacion con fastapi",
+    version="0.0.1"
+)
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
